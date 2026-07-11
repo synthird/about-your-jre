8cd899047650d1c54971eb591dfe03e42b3cc636
Synthird <105394020+synthird@users.noreply.github.com>
2026-07-11 13:24:47 -0700

    feat: set up JFrame

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,5 +8,11 @@ public class MainFrame extends JFrame {
 		System.out.println(vendorWebsite);
 		System.out.println(deviceOS);
 		System.out.println(bugReportLink);
+
+		this.setSize(333, 290);
+		this.setTitle("About your JRE");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
 	}
 }
