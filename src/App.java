e11582184850997d482395e3fe739de2313bf0b2
Synthird <105394020+synthird@users.noreply.github.com>
2026-07-12 16:25:08 -0700

    feat(cli): display JRE info and add more hyphens to the heading

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
 public class App {
 	public static void main(String[] args) {
 		JREInfo jreInfo = new JREInfo();
+		String jreInfoDisplay = String.format(
+				"Version: %s\nVersion date: %s\nVendor: %s\nVendor website: %s\nDevice operating system: %s\nReport JRE bugs: %s",
+				jreInfo.version,
+				jreInfo.versionDate,
+				jreInfo.vendor,
+				jreInfo.vendorSite,
+				jreInfo.deviceOS,
+				jreInfo.bugReportLink);
 
-		System.out.println("--- ABOUT YOUR JRE ---");
+		System.out.println("----- ABOUT YOUR JRE -----");
+		System.out.println(jreInfoDisplay);
 
 		new MainFrame(jreInfo);
 	}
