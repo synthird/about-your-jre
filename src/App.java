befdabb1979339bd6d7c5da3c1850304d9d15de7
Synthird <105394020+synthird@users.noreply.github.com>
2026-07-12 16:18:08 -0700

    feat(cli): add a heading

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ public class App {
 	public static void main(String[] args) {
 		JREInfo jreInfo = new JREInfo();
 
+		System.out.println("--- ABOUT YOUR JRE ---");
+
 		new MainFrame(jreInfo);
 	}
 }
