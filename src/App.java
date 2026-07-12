da4f3904e2f68c9baebeae5253e311b2671ec8f7
Synthird <105394020+synthird@users.noreply.github.com>
2026-07-12 16:42:54 -0700

    feat: only display the GUI if there is no argument entered in the terminal

--- src/App.java
+++ src/App.java
@@ -1,18 +1,21 @@
 public class App {
 	public static void main(String[] args) {
 		JREInfo jreInfo = new JREInfo();
-		String jreInfoDisplay = String.format(
-				"Version: %s\nVersion date: %s\nVendor: %s\nVendor website: %s\nDevice operating system: %s\nReport JRE bugs: %s",
-				jreInfo.version,
-				jreInfo.versionDate,
-				jreInfo.vendor,
-				jreInfo.vendorSite,
-				jreInfo.deviceOS,
-				jreInfo.bugReportLink);
 
-		System.out.println("----- ABOUT YOUR JRE -----");
-		System.out.println(jreInfoDisplay);
+		if (args.length == 0) {
+			new MainFrame(jreInfo);
+		} else {
+			String jreInfoDisplay = String.format(
+					"Version: %s\nVersion date: %s\nVendor: %s\nVendor website: %s\nDevice operating system: %s\nReport JRE bugs: %s",
+					jreInfo.version,
+					jreInfo.versionDate,
+					jreInfo.vendor,
+					jreInfo.vendorSite,
+					jreInfo.deviceOS,
+					jreInfo.bugReportLink);
 
-		new MainFrame(jreInfo);
+			System.out.println("----- ABOUT YOUR JRE -----");
+			System.out.println(jreInfoDisplay);
+		}
 	}
 }
